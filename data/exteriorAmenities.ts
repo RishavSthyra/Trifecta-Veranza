a40a70f76a438b685dec0846ce6a19c14bb093b9
RishavSthyra <rishav@sthyra.com>
2026-04-08 10:14:41 +0530

    minimap updated

--- data/exteriorAmenities.ts
+++ data/exteriorAmenities.ts
@@ -394,7 +394,7 @@ const amenityDefinitions: ExteriorAmenityDefinition[] = [
 ];
 
 export const EXTERIOR_MINIMAP_IMAGE_URL =
-  "https://cdn.sthyra.com/images/TopRender_1i.jpg";
+  "https://cdn.sthyra.com/images/Final_1.avif";
 
 export const EXTERIOR_MINIMAP_BOUNDS = {
   bottomLeft: {
